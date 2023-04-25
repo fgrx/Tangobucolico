2a6864d8003d872106092ed7138cb864e082b81a
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-04-25 11:16:02 +0200

    Ajout de blocs sur la home

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#65a30d",
+        primaryMedium: "#b0cf76",
+        primaryLight: "#dce6ca",
+        secondary: "#b91c1c",
+        secondaryLight: "#fecaca",
+        backgroundDarker: "#fffbeb",
+        background: "#fdfaee",
+        dark: "#2f2d2e",
+        light: "#fff",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
